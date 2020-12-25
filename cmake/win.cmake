d66848c434d768cbaa9c066d673cfbfc63c6cea4
picam360 <picam360@ohmydigifab.com>
2020-12-25 09:51:59 +0900

    mingw

--- cmake/win.cmake
+++ cmake/win.cmake
@@ -1,6 +1,21 @@
 
 set(TARGET_ARCH "x86_64")
 
+add_custom_command(
+        TARGET ${PROJECT_NAME} PRE_BUILD
+        COMMAND ${CMAKE_COMMAND} -E copy
+                ${CMAKE_CURRENT_SOURCE_DIR}/lib/win/pstcore/lib/picam360/${TARGET_ARCH}/libpstcore.dll
+                ${CMAKE_CURRENT_BINARY_DIR}/Release/libpstcore.dll
+)
+file(GLOB MING_DLLS ${CMAKE_CURRENT_SOURCE_DIR}/lib/win/mingw/*.dll)
+foreach(_DLL_FILE IN LISTS MING_DLLS)
+    add_custom_command(
+                TARGET ${PROJECT_NAME} PRE_BUILD
+                COMMAND ${CMAKE_COMMAND} -E copy
+                        ${_DLL_FILE}
+                        ${CMAKE_CURRENT_BINARY_DIR}/Release/)
+endforeach()
+
 target_link_directories(${PROJECT_NAME} PRIVATE
 )
 
