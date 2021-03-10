2a40d300fefd9ff71be6e45f2304dd53b95abdcd
takuma <takuma@localhost>
2021-03-10 10:24:45 +0900

    0.0.19

--- lib/osx/pstcore/include/picam360/pcommon/popen2.h
+++ lib/osx/pstcore/include/picam360/pcommon/popen2.h
@@ -7,8 +7,11 @@ extern "C" {
 
 #endif
 
-
-int popen2(const char *def, int *fd_r, int *fd_w, int *fd_exit);
+typedef void* PCPIPE_T;
+int pcpipe_open(const char *def, PCPIPE_T *fd_r, PCPIPE_T *fd_w, PCPIPE_T *fd_exit);
+int pcpipe_close(PCPIPE_T fd);
+int pcpipe_read(PCPIPE_T fd, void* ptr, int size);
+int pcpipe_write(PCPIPE_T fd, const void* ptr, int size);
 
 
 #ifdef __cplusplus
