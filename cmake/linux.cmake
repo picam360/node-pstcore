e00b2958b541fbe5e70206b5d2b4dc8707a7f9c7
hirayama.takuma <hirayama.takuma@ohmydigifab.com>
2021-09-23 00:27:10 +0900

    jetson

--- cmake/linux.cmake
+++ cmake/linux.cmake
@@ -14,11 +14,20 @@ endif ()
 
 find_package(PkgConfig REQUIRED)
 
+#search path $ORIGIN first
+set(CMAKE_SHARED_LINKER_FLAGS "-Wl,-rpath,$ORIGIN")
+
 add_custom_command(
         TARGET ${PROJECT_NAME} PRE_BUILD
         COMMAND ${CMAKE_COMMAND} -E copy
                 ${CMAKE_CURRENT_SOURCE_DIR}/lib/${PSTCOREDIR}/pstcore/lib/picam360/${TARGET_ARCH}/libpstcore.so
                 ${CMAKE_CURRENT_BINARY_DIR}/Release/libpstcore.so
+        COMMAND ${CMAKE_COMMAND} -E copy
+                ${CMAKE_CURRENT_SOURCE_DIR}/lib/${PSTCOREDIR}/depends/${TARGET_ARCH}/libtoupcam.so
+                ${CMAKE_CURRENT_BINARY_DIR}/Release/libtoupcam.so
+        COMMAND ${CMAKE_COMMAND} -E copy
+                ${CMAKE_CURRENT_SOURCE_DIR}/lib/${PSTCOREDIR}/depends/${TARGET_ARCH}/libnncam.so
+                ${CMAKE_CURRENT_BINARY_DIR}/Release/libnncam.so
 )
 
 #target_link_directories(${PROJECT_NAME} PRIVATE
