c9095d883457040405ae2ce6ec75bf551cd9fde2
hirayama.takuma <hirayama.takuma@ohmydigifab.com>
2022-02-28 23:02:40 +0900

    jetson update

--- lib/jetson/pstcore/include/picam360/pcommon/pvf_loader.h
+++ lib/jetson/pstcore/include/picam360/pcommon/pvf_loader.h
@@ -19,13 +19,16 @@ extern "C" {
 #endif
 
 typedef void *PVFLDR_T;
-typedef void (*PVFLDR_CALLBACK)(unsigned char *data, int size, void *arg);
+typedef void (*PVFLDR_CALLBACK)(unsigned char *data, int size, bool is_frame, int n_frame, void *arg);
 
 PVFLDR_T FUNC_DEF pvfldr_new(const char *url, const char *head_query, const char *get_query, PVFLDR_CALLBACK callback,
 		void *arg);
 void FUNC_DEF pvfldr_delete(PVFLDR_T _this);
-void FUNC_DEF pvfldr_load_next(PVFLDR_T _this, float *view_quat);
-void FUNC_DEF pvfldr_reset(PVFLDR_T _this);
+void FUNC_DEF pvfldr_poll(PVFLDR_T _this);
+void FUNC_DEF pvfldr_load(PVFLDR_T _this, int pitch, int yaw, int frame_cur, int n_frames);
+
+float FUNC_DEF pvfldr_get_kf_latency_ave(PVFLDR_T _this);
+float FUNC_DEF pvfldr_get_kf_latency_sigma(PVFLDR_T _this);
 
 #ifdef __cplusplus
 
