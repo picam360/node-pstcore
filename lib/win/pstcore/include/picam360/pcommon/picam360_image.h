736d00bc438fb11741304f2837c367948f80b455
hirayama.takuma <hirayama.takuma>
2022-08-29 16:08:02 +0900

    v0.2.1 : win

--- lib/win/pstcore/include/picam360/pcommon/picam360_image.h
+++ lib/win/pstcore/include/picam360/pcommon/picam360_image.h
@@ -15,7 +15,7 @@ typedef struct _PICAM360_IMAGE_T {
 	REFERENCE_H *ref;
 
 	unsigned char uuid[16];
-	char mem_type[5]; //"CUDA"
+	char mem_type[5]; //"CPU"|"GPU"|"GL"|"CUDA"|"VCSM"
 	char img_type[5]; //"RGB"|"RGBA"|"I420"|"JPEG"|"H264"|"VP8"
 	struct timeval timestamp;
 
@@ -28,7 +28,7 @@ typedef struct _PICAM360_IMAGE_T {
 	unsigned int height[MAX_NUM_OF_PLANES];
 	unsigned int stride[MAX_NUM_OF_PLANES];
 	unsigned char *pixels[MAX_NUM_OF_PLANES]; //size should be stride*height
-	int id[MAX_NUM_OF_PLANES]; //PICAM360_MEMORY_TYPE_EGL
+	intptr_t ext[MAX_NUM_OF_PLANES]; //external data
 } PICAM360_IMAGE_T;
 
 int save_picam360_image_header(PICAM360_IMAGE_T *image,int idx,
--- lib/win/pstcore/include/picam360/pcommon/pvf_loader.h
+++ lib/win/pstcore/include/picam360/pcommon/pvf_loader.h
@@ -19,13 +19,17 @@ extern "C" {
 #endif
 
 typedef void *PVFLDR_T;
-typedef void (*PVFLDR_CALLBACK)(unsigned char *data, int size, void *arg);
+typedef void (*PVFLDR_CALLBACK)(unsigned char *data, int size, bool is_frame, int n_frame, void *arg);
 
-PVFLDR_T FUNC_DEF pvfldr_new(const char *url, const char *head_query, const char *get_query, PVFLDR_CALLBACK callback,
+void FUNC_DEF pvfldr_new(PVFLDR_T *out_obj, const char *url, const char *head_query, const char *get_query, PVFLDR_CALLBACK callback,
 		void *arg);
 void FUNC_DEF pvfldr_delete(PVFLDR_T _this);
-void FUNC_DEF pvfldr_load_next(PVFLDR_T _this, float *view_quat);
-void FUNC_DEF pvfldr_reset(PVFLDR_T _this);
+void FUNC_DEF pvfldr_poll(PVFLDR_T _this);
+void FUNC_DEF pvfldr_load(PVFLDR_T _this, int pitch, int yaw, int frame_cur, int n_frames);
+void FUNC_DEF pvfldr_get_pos(PVFLDR_T _this, const char *path, char *out_url, uint64_t *out_offset, int *out_length);
+
+float FUNC_DEF pvfldr_get_kf_latency_ave(PVFLDR_T _this);
+float FUNC_DEF pvfldr_get_kf_latency_sigma(PVFLDR_T _this);
 
 #ifdef __cplusplus
 
